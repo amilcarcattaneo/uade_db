ccb28386741a23cad87de03731c1ea77b3625940
amilcarcattaneo <amilcar.cattaneo@mercadolibre.com>
2020-05-31 19:58:06 -0300

    Procedimientos terminados

--- db/Entrega03/procedimientos.sql
+++ db/Entrega03/procedimientos.sql
@@ -50,6 +50,58 @@ BEGIN
     END
 END
 
+-- ---------------------- --
+-- Actualizar un turno --
+-- ---------------------- --
+
+CREATE PROCEDURE actualizarTurno
+			@idTurno	    INT,
+			@fechaTurno		DATETIME,
+			@estado			INT,
+            @observacion    VARCHAR(50)
+AS
+BEGIN
+    IF NOT EXISTS(SELECT * FROM turno WHERE idTurno = @idTurno)
+    BEGIN
+        PRINT 'El turno no existe.'
+        RETURN
+    END
+	ELSE
+	BEGIN
+		UPDATE Turno SET fechaTurno = @fechaTurno, estado = @estado, observacion = @observacion WHERE idTurno = @idTurno
+		PRINT 'El Turno se actualizó correctamente.'
+		RETURN
+	END
+END
+
+-- ---------------------- --
+-- Actualizar un paciente --
+-- ---------------------- --
+
+CREATE PROCEDURE actualizarPaciente
+			@idPaciente		INT,
+			@nombre			VARCHAR(50),
+			@apellido		VARCHAR(50),
+			@fnacimiento	DATE,
+			@domicilio		VARCHAR(50),
+			@idpais			CHAR(3),
+			@tel			VARCHAR(20),
+			@email			VARCHAR(30),
+			@observacion	VARCHAR(1000)
+AS
+BEGIN
+    IF NOT EXISTS(SELECT * FROM Paciente WHERE idPaciente = @idPaciente)
+    BEGIN
+        PRINT 'El paciente no existe.'
+        RETURN
+    END
+	ELSE
+	BEGIN
+		UPDATE Paciente SET nombre = @nombre, apellido = @apellido, fNacimiento = @fnacimiento, domicilio = @domicilio, idPais = @idpais, telefono = @tel, email = @email, observacion = @observacion WHERE idPaciente = @idPaciente
+		PRINT 'El paciente se actualizó correctamente.'
+		RETURN
+	END
+END
 
 -- ----------------- --
 -- Eliminar un turno --
@@ -68,6 +120,7 @@ BEGIN
     BEGIN
         -- borrado lógico asumiendo que el estado 1 es 'eliminado'
         UPDATE turno SET estado = 1 WHERE idTurno = @idTurno
+        PRINT 'El turno se eliminó correctamente.'
         RETURN
     END
 END
